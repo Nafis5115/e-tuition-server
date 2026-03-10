ace7e6e6d81ed316c090021981f2a7061563c9d8
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-03-10 15:29:03 +0600

    completed saving users in db

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -11,8 +11,13 @@ export const getUsers = async (req, res) => {
 
 export const createUser = async (req, res) => {
   try {
+    const { email } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(200).json({ message: "User already in database" });
+    }
     const user = await User.create(req.body);
-    res.send(user);
+    res.status(201).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
