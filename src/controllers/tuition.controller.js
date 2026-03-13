0b9917c450003e13f14cadc92221a7d87459092b
Nafis5115 <72686804+Nafis5115@users.noreply.github.com>
2026-03-14 01:03:35 +0600

    ongoing all-tuitions

--- src/controllers/tuition.controller.js
+++ src/controllers/tuition.controller.js
@@ -52,8 +52,35 @@ export const getTuitionDetails = async (req, res) => {
 export const getAllTuitions = async (req, res) => {
   try {
     // const query = { status: "approved" };
-    const tuitions = await Tuition.find();
-    res.status(200).json(tuitions);
+    const { class: classFilter, sort, search } = req.query;
+    const query = {};
+    if (classFilter) {
+      query.class = classFilter;
+    }
+    if (search) {
+      query.subject = { $regex: search, $options: "i" };
+    }
+    let sortOption = { createdAt: -1 };
+    if (sort === "budget-asc") {
+      sortOption = { budget: 1 };
+    }
+    if (sort === "budget-desc") {
+      sortOption = { budget: -1 };
+    }
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 8;
+    const skip = (page - 1) * limit;
+    const tuitions = await Tuition.find(query)
+      .skip(skip)
+      .limit(limit)
+      .sort(sortOption);
+    const total = await Tuition.countDocuments();
+    res.status(200).json({
+      tuitions,
+      page,
+      total,
+      totalPages: Math.ceil(total / limit),
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
